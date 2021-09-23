b1eb45d165070596bcb44a199828b919ac0612e9
dzhikrokhatunnafilaa <dznafila8@gmail.com>
2021-09-23 10:24:55 +0700

    upload file

---
+++ app/Controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php namespace App\Controllers;
+
+class Contoh1 extends BaseController
+{
+	public function index()
+	{
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Dzhikrokhatun Nafila
+ Saya tinggal di daerah jakarta
+ olahraga yang saya sukai adalah Badminton";
+ }
+}
+?>
